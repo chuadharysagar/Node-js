86ec61a8e0f7acb702fb3f9dfd9198dc80462fa9
chaudharysagar <sagarchaudharynn06@gmail.com>
2024-09-02 15:22:16 +0530

    new content added in both file

---
+++ 7-path-modules.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+console.log(path.basename("home/folder/user/me"));
+console.log(path.dirname("home/folder/user/me"));
+
+console.log(path.join("home/folder/user/me.js"))
+console.log(path.extname("home/folder/user/me"));
--- 8-http-modul.js
+++ 8-http-modul.js
@@ -3,4 +3,4 @@ const http = require("http");
 http.createServer((req,resp)=>{
    resp.write("<h1>hello this is sagar chaudhary</h1>");
    resp.end();
-}).listen(4500);
+}).listen(4500,()=>console.log("Server is running at 4500 port"));
