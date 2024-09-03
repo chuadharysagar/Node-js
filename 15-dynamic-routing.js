5eed48c71ff50ec3534edf0aadac22d5bc309fd4
chaudharysagar <sagarchaudharynn06@gmail.com>
2024-09-03 16:13:23 +0530

    dynamic page redering using EJS tenplate engine

---
+++ 15-dynamic-routing.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const filePath = path.join(__dirname,"public");
+
+// EJS Engine to create the dynamic pages and inject the data to html by server
+// Embeded javascript (ejs)
+app.set('view engine','ejs');
+
+app.get('/',(_,resp)=>{
+    resp.sendFile(`${filePath}/index.html`);
+});
+
+
+app.get('/profile',(_,resp)=>{
+    const user={
+        name :"sagar",
+        email:"sagar@gmail.com",
+        age:21,
+    }
+    resp.render('profile',{user});
+})
+
+
+//Ejs file rendering
+
+app.listen(5000,()=>{
+    console.log("server is running on port 5000");
+});
+
